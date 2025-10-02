219096d521dffd0ccd0be2709f00b514984e1d87
Saheena <saheena20022002@gmail.com>
2025-10-02 20:33:10 +0530

    fifth commit

---
+++ git/src/git/Git.java
@@ -0,0 +1,18 @@
+package git;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Git {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\gauth\\Downloads\\geckodriver-v0.26.0-win64\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("https://ecommerce-playground.lambdatest.io/index.php?route=account/login");
+		
+		driver.findElement(By.name("email")).sendKeys("Saheena@123gmail.com");
+		
+		driver.findElement(By.id("input-password")).sendKeys("Saheena@123");
+
+}}
